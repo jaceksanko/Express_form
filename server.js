f53c96fc6a20c06dfee98fe7bbd9d91c598044a7
jaceksanko <jacek.sanko@gmail.com>
2018-04-25 19:10:02 +0200

    Add simple form

--- server.js
+++ server.js
@@ -15,32 +15,6 @@ app.get('/userform', function(req, res){
     res.end(JSON.stringify(response));
 });
 
-/* app.get('/:id', function(req, res){
-    console.log('Otrzymałem żądanie POST do strony głównej');
-    res.send('Identyfikator, który został dopisany to ' + req.params.id);
-}); */
-
-/* app.delete('/del_user', function (req, res) {
-    console.log('Otrzymałem żądanie DELETE do strony /del_user');
-    res.send('Hello DELETE!');
-});
-
-app.get('/list_user', function (req, res) {
-    console.log('Otrzymałem żądanie GET do strony /list_user');
-    res.send('Strona z listą użytkowników!');
-});
-
-app.get('/ab*cd', function(req, res) {
-    console.log('Otrzymałem żądanie GET do strony /ab*cd');
-    res.send('Wzór pasuje');
-}); */
-
-/* app.listen(3000); */
-
-/* app.use(function (req, res, next){
-    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
-}); */
-
 var server = app.listen(3000, 'localhost', function() {
     var host = server.address().address;
     var port = server.address().port;
